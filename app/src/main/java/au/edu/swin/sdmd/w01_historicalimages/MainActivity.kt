9facc50dd4b2f782f4c44e19a728d248e947283c
Duc Minh Le <lemduc@gmail.com>
2024-01-18 09:32:55 +0700

    demo listeners

--- app/src/main/java/au/edu/swin/sdmd/w01_historicalimages/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w01_historicalimages/MainActivity.kt
@@ -1,11 +1,24 @@
 package au.edu.swin.sdmd.w01_historicalimages
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnAbout = findViewById<Button>(R.id.btnAbout)
+        btnAbout.setOnClickListener{
+            displayAbout(it)
+        }
+    }
+
+    fun displayAbout(v: View) {
+        Toast.makeText(v.context, "Historical Image App - 2024", Toast.LENGTH_LONG).show()
     }
 }
